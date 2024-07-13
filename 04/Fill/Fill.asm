85ccb9a37d68cd15d3c4c6f3c9938f34081690fc
lalit <lalitvijay9480@gmail.com>
2024-07-13 23:10:36 +0530

    finish mult and fill assembly code

--- 04/Fill/Fill.asm
+++ 04/Fill/Fill.asm
@@ -3,9 +3,51 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/4/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input. 
+// Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel. When no key is pressed, 
+// i.e. writes "black" in every pixel. When no key is pressed,
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+(LOOP)
+  @8192
+  D=A
+  @i
+  M=D
+  @KBD
+  D=M
+  @START
+  D;JNE
+  @WHITE
+  D;JEQ
+
+(START)
+  @SCREEN
+  D=A
+  @i
+  D=D+M
+  A=D
+  M=-1
+  @i
+  D=M
+  @LOOP
+  D;JEQ
+  @i
+  M=M-1
+  @START
+  0;JMP
+
+(WHITE)
+  @SCREEN
+  D=A
+  @i
+  D=D+M
+  A=D
+  M=0
+  @i
+  D=M
+  @LOOP
+  D;JEQ
+  @i
+  M=M-1
+  @WHITE
+  0;JMP
--- 04/Mult/Mult.asm
+++ 04/Mult/Mult.asm
@@ -3,7 +3,74 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/4/Mult.asm
 
-// Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+// Multiplies 0 and 1 and stores the result in 2.
+// (0, 1, 2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
+// A - register address
+// M - RAM[A]
+// D - temp 16-bit
+// add ram[0], ram[1] times
+
+
+// @i
+// M=0
+// @R2
+// M=0
+//
+// (LOOP)
+//   @i
+//   D=M
+//   @R1
+//   D=D-M
+//   @END
+//   D;JGT
+//   @i
+//   D=M
+//  @R2
+//  M=D+M
+//   @i
+//   M=M+1
+//   @LOOP
+//   0;JMP
+
+// (END)
+//   @END
+//   0;JMP
+
+
+// @R2
+
+
+// let r3 = 0
+// LOOP:
+//   if r2 = 0; goto STOP
+//   r3=r3+r1
+//   r2=r2-1
+//   goto loop
+// STOP:
+//   infinite loop
+
+
+@R2
+M=0
+
+(LOOP)
+  @R1
+  D=M
+  @END
+  D;JEQ
+
+  @R0
+  D=M
+  @R2
+  M=D+M
+
+  @R1
+  M=M-1
+  @LOOP
+  0;JMP
+
+(END)
+  @END
+  0;JMP
